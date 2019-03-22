a29528f7aff2d32ec7077105202e5c8919438203
Akkusaii <mo0307b1006@gmail.com>
2019-03-22 13:25:43 +0800

    add hw4_00

---
+++ 2019_spring/hw/hw04/hw4_00.py
@@ -0,0 +1,31 @@
+problem = int(input())
+
+if (problem == 1):
+    print(1)
+
+if (problem == 2):
+    print(0)
+
+if (problem == 3):
+    print(0)
+
+if (problem == 4):
+    print(0)
+
+if (problem == 5):
+    print(0)
+
+if (problem == 6):
+    print(0)
+
+if (problem == 7):
+    print(0)
+
+if (problem == 8):
+    print(1) #not confirm
+
+if (problem == 9):
+    print(0)
+
+if (problem == 10):
+    print(1)
